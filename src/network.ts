be76106114ab2733dcb29656e2018a91f39b1ccc
Navdeep Garg <navdeep@revinfotech.com>
2022-11-16 10:49:43 +0000

    mumbai region 16-11-22

--- src/network.ts
+++ src/network.ts
@@ -1,5 +1,5 @@
 export const network: string = 'goerli'
-export const stakingNftAddress: string = '0x717FAC081af6baBe74549AdAC7C0b286f95a05c7'
-export const registryAddress: string = '0x20f9999E97c0B5A212E4D5BE164C8e1d6BD95Eda'
+export const stakingNftAddress: string = '0x4fdfaF352A3B1F6Ad302F21Df182272cCE3cdE8e'
+export const registryAddress: string = '0x0C756D14AC222a195f4331A822CFe10284236012'
 export const decoderForPoSPortalData: string = '0x8fcFA769D420AdA837206294eb054Ad514a7a8a0'
 
