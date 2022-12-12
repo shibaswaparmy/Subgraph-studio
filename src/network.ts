b80476b60bc8ba94a7ae08ff19f91bbad555de8b
Robil <robil@eiysys.in>
2022-12-12 14:33:16 +0530

    address updated

--- src/network.ts
+++ src/network.ts
@@ -1,5 +1,5 @@
 export const network: string = 'goerli'
-export const stakingNftAddress: string = '0x4fdfaF352A3B1F6Ad302F21Df182272cCE3cdE8e'
-export const registryAddress: string = '0x0C756D14AC222a195f4331A822CFe10284236012'
+export const stakingNftAddress: string = '0x775bC36645d2232F25d5c14780E2b66FFd9bF50B'
+export const registryAddress: string = '0xc544fa2687b22F06140d11Bb337cc063998a726C'
 export const decoderForPoSPortalData: string = '0x8fcFA769D420AdA837206294eb054Ad514a7a8a0'
 
